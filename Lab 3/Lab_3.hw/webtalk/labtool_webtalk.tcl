82c86a29aa9b27d2a5862bcd1ff1343e6b662421
Ian McElhenny <mcelhenny_ian@yahoo.com>
2015-09-24 23:53:16 -0400

    lab 3 stuff (extra)

--- Lab 3/Lab_3.hw/webtalk/labtool_webtalk.tcl
+++ Lab 3/Lab_3.hw/webtalk/labtool_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir F:/CEC330/Lab 3/Lab_3.hw/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Sep 21 23:05:22 2015" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Sep 21 23:48:55 2015" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "Vivado v2015.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "1266856" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -24,6 +24,6 @@ webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "use
 webtalk_register_client -client labtool
 webtalk_add_data -client labtool -key cable -value "Digilent/Nexys4DDR/15000000" -context "labtool\\usage"
 webtalk_add_data -client labtool -key chain -value "13631093" -context "labtool\\usage"
-webtalk_add_data -client labtool -key pgmcnt -value "03:00:00" -context "labtool\\usage"
-webtalk_transmit -clientid 824745796 -regid "211068406_0_0_244" -xml F:/CEC330/Lab 3/Lab_3.hw/webtalk/usage_statistics_ext_labtool.xml -html F:/CEC330/Lab 3/Lab_3.hw/webtalk/usage_statistics_ext_labtool.html -wdm F:/CEC330/Lab 3/Lab_3.hw/webtalk/usage_statistics_ext_labtool.wdm -intro "<H3>LABTOOL Usage Report</H3><BR>"
+webtalk_add_data -client labtool -key pgmcnt -value "01:00:00" -context "labtool\\usage"
+webtalk_transmit -clientid 2073286632 -regid "211068406_0_0_244" -xml F:/CEC330/Lab 3/Lab_3.hw/webtalk/usage_statistics_ext_labtool.xml -html F:/CEC330/Lab 3/Lab_3.hw/webtalk/usage_statistics_ext_labtool.html -wdm F:/CEC330/Lab 3/Lab_3.hw/webtalk/usage_statistics_ext_labtool.wdm -intro "<H3>LABTOOL Usage Report</H3><BR>"
 webtalk_terminate
