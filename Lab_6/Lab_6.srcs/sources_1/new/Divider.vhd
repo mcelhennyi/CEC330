f83cc75b93a27b6e642d7b0404bc1b7fd197e827
Austin Musser <mussera@my.erau.edu>
2015-10-22 18:00:56 -0400

    no message

--- Lab_6/Lab_6.srcs/sources_1/new/Divider.vhd
+++ Lab_6/Lab_6.srcs/sources_1/new/Divider.vhd
@@ -44,24 +44,15 @@ architecture Behavioral of Divider is
 
 signal register_counter : STD_LOGIC_VECTOR (26 DOWNTO 0) := "000" & x"000000";
 
+signal counter1Hz : STD_LOGIC_VECTOR (31 DOWNTO 0) := x"00000000";
+signal clk_out1Hz : STD_LOGIC := 0;
+
 signal counter16Hz : STD_LOGIC_VECTOR (23 DOWNTO 0) := x"000000";
 signal clk_out16Hz : STD_LOGIC := 0;
 
 begin
 
---counts up every clock for a clock divider
-counter: process(CLK_IN)
-    begin
-        if (rising_edge(CLK_IN)) then
-            register_counter <= register_counter +1;
-        end if;
-    end process counter;
-    
-CLK_OUT_SLOW <= register_counter(26);--Slow 1Hz clock
-CLK_OUT_AN <= register_counter(14);--About 70Hz for display switching
-CLK_OUT_STATE <= register_counter(4); --Counter to change a state machine
-
---counts up every clock for a clock divider
+--16Hz clock
 counter: process(CLK_IN)
     begin
         if (rising_edge(CLK_IN)) then
@@ -75,5 +66,25 @@ counter: process(CLK_IN)
             
         end if;
     end process counter;
+    
+--1Hz clock
+    counter: process(CLK_IN)
+        begin
+            if (rising_edge(CLK_IN)) then
+                register_counter <= register_counter +1;
+                if counter1Hz = "00011101110011010110010100000000" then
+                    clk_out1Hz = '1';
+                elsif counter1Hz = "00111011100110101100101000000000" then
+                    clk_out1Hz = '0';
+                    counter1Hz = x"00000000";
+                end if;
+                
+            end if;
+        end process counter;
+        
+CLK_OUT_SLOW <= clk_out1Hz;--Slow 1Hz clock
+
+CLK_OUT_AN <= register_counter(14);--About 70Hz for display switching
+CLK_OUT_STATE <= register_counter(4); --Counter to change a state machine
 
 end Behavioral;
