c7bd806ca6247003cadfff0c99d83feb656d2486
Ian McElhenny <mcelhenny_ian@yahoo.com>
2015-10-25 19:20:30 -0400

    Added a standalone PWM module for testing. Operable except for the tens place of the number showing up.

--- Lab_6/Lab_6.srcs/sources_1/new/Divider.vhd
+++ Lab_6/Lab_6.srcs/sources_1/new/Divider.vhd
@@ -36,6 +36,7 @@ entity Divider is
     Port ( CLK_IN : in STD_LOGIC;
            CLK_OUT_1Hz : out STD_LOGIC;
            CLK_OUT_16Hz : out STD_LOGIC;
+           CLK_OUT_50Hz :  out STD_LOGIC;
            CLK_OUT_AN : out STD_LOGIC;
            CLK_OUT_STATE : out STD_LOGIC
            );
@@ -45,73 +46,63 @@ architecture Behavioral of Divider is
 
 signal register_counter : STD_LOGIC_VECTOR (26 DOWNTO 0) := "000" & x"000000";
 
-signal counter1Hz : STD_LOGIC_VECTOR (31 DOWNTO 0) := x"00000000";
+signal counter1Hz : STD_LOGIC_VECTOR (26 DOWNTO 0) := "000" & x"000000";
 signal clk_out1Hz : STD_LOGIC := '0';
 
 signal counter16Hz : STD_LOGIC_VECTOR (23 DOWNTO 0) := x"000000";
 signal clk_out16Hz : STD_LOGIC := '0';
 
+signal counter50Hz : STD_LOGIC_VECTOR (12 DOWNTO 0) := "0000000000000";
+signal clk_out50Hz : STD_LOGIC := '0';
+
 begin
-
-
-counter1: process(CLK_IN)
+--50Hz clock
+counter50: process(CLK_IN)
     begin
-        --clock for AN and STATE
         if (rising_edge(CLK_IN)) then
-            register_counter <= register_counter + 1;
+            counter50Hz <= counter50Hz +1;
+            if counter50Hz = "0111101000010" then
+                clk_out50Hz <= '1';
+            elsif counter50Hz = "1111010000100" then
+                clk_out50Hz <= '0';
+                counter50Hz <= "0000000000000";
+            end if;
         end if;
-    end process counter1;
+end process counter50;
+
+--16Hz clock
+counter16: process(CLK_IN)
+    begin
+        if (rising_edge(CLK_IN)) then
+            counter16Hz <= counter16Hz +1;
+            if counter16Hz = "001011111010111100001000" then
+                clk_out16Hz <= '1';
+            elsif counter16Hz = "010111110101111000010000" then
+                clk_out16Hz <= '0';
+                counter16Hz <= x"000000";
+            end if;
+            
+        end if;
+    end process counter16;
     
-counter2: process(CLK_IN)
-    begin
-        --16Hz clock
-        if (rising_edge(CLK_IN)) then
-            counter16Hz <= counter16Hz + 1;
-        end if;
-        if counter16Hz = "001011111010111100001000" then
-            clk_out16Hz <= '1';
-        elsif counter16Hz = "010111110101111000010000" then
-            clk_out16Hz <= '0';
-            counter16Hz <= x"000000";
-        end if;
-    end process counter2;
---counter2_16Hz: process(counter16Hz)
---    begin
---        if counter16Hz = "001011111010111100001000" then
---            clk_out16Hz <= '1';
---        elsif counter16Hz = "010111110101111000010000" then
---            clk_out16Hz <= '0';
---            counter16Hz <= x"000000";
---        end if;
---    end process counter2_16Hz;
-        
-counter3: process(CLK_IN)
-    begin
-        --1Hz clock
-        if (rising_edge(CLK_IN)) then
-            counter1Hz <= counter1Hz + 1;
-        end if;
-        if counter1Hz = "00011101110011010110010100000000" then
-            clk_out1Hz <= '1';
-        elsif counter1Hz = "00111011100110101100101000000000" then
-            clk_out1Hz <= '0';
-            counter1Hz <= x"00000000";
-        end if;
-    end process counter3;
---counter3_1Hz: process(counter1Hz)
---    begin
---        if counter1Hz = "00011101110011010110010100000000" then
---            clk_out1Hz <= '1';
---        elsif counter1Hz = "00111011100110101100101000000000" then
---            clk_out1Hz <= '0';
---            counter1Hz <= x"00000000";
---        end if;
---    end process counter3_1Hz;
+--1Hz clock
+counter1: process(CLK_IN)
+        begin
+            if (rising_edge(CLK_IN)) then
+                counter1Hz <= counter1Hz +1;
+                if counter1Hz = "010111110101111000010000000" then
+                    clk_out1Hz <= '1';
+                elsif counter1Hz = "101111101011110000100000000" then
+                    clk_out1Hz <= '0';
+                    counter1Hz <= "000" & x"000000";
+                end if;
+                
+            end if;
+        end process counter1;
         
 CLK_OUT_1Hz <= clk_out1Hz;--Slow 1Hz clock
-CLK_OUT_16Hz <= clk_out16Hz;--Slow 1Hz clock
---CLK_OUT_1Hz <= register_counter(26);
---CLK_OUT_16Hz <= register_counter(18);
+CLK_OUT_16Hz <= clk_out16Hz;--16Hz clock
+CLK_OUT_50Hz <= clk_out50Hz; --PWM refresh clock for LEDS
 CLK_OUT_AN <= register_counter(14);--About 70Hz for display switching
 CLK_OUT_STATE <= register_counter(4); --Counter to change a state machine
 
--- Lab_6/Lab_6.srcs/sources_1/new/Top_countdown.vhd
+++ Lab_6/Lab_6.srcs/sources_1/new/Top_countdown.vhd
@@ -58,6 +58,7 @@ signal Disp8 : STD_LOGIC_VECTOR (3 downto 0);
 
 signal clk_slow : STD_LOGIC := '0'; --The one Hz clock
 signal clk_16Hz : STD_LOGIC := '0'; --16Hz clock
+signal clk_50Hz : STD_LOGIC := '0'; --16Hz clock
 signal clk_an : STD_LOGIC; --Clock that is around 70Hz going to the annodes and cathode counter
 signal clk_state : STD_LOGIC; --Clock to change State Machine
 signal pwm_clk : STD_LOGIC; --Clock that goes to PWM module
@@ -65,6 +66,8 @@ signal pwm_clk : STD_LOGIC; --Clock that goes to PWM module
 signal pwm_level : STD_LOGIC_VECTOR (7 DOWNTO 0) := x"00"; --level of pwm
 signal pwm_out : STD_LOGIC; --one bit value for pwm signal to LEDs
 signal timer : STD_LOGIC_VECTOR (4 DOWNTO 0) := "00000"; --count down timer variable
+
+signal button_center : STD_LOGIC;
 
 -----------------------------------
 --COMPONETS------------------------
@@ -74,6 +77,7 @@ component Divider
     Port ( CLK_IN : in STD_LOGIC;
            CLK_OUT_1Hz : out STD_LOGIC;
            CLK_OUT_16Hz : out STD_LOGIC;
+           CLK_OUT_50Hz :  out STD_LOGIC;
            CLK_OUT_AN : out STD_LOGIC;
            CLK_OUT_STATE : out STD_LOGIC
            );
@@ -111,6 +115,9 @@ component Binary_to_decimal
 end component Binary_to_decimal;
 
 begin
+button_center <= BTNC;
+--timer <= "0000" & clk_slow;
+
 ------------------------------------
 --PORT MAPS-------------------------
 ------------------------------------
@@ -119,12 +126,13 @@ divider_map: Divider
     port map ( CLK_IN  => CLK_IN,
                CLK_OUT_1Hz => clk_slow,
                CLK_OUT_16Hz => clk_16Hz,
+               CLK_OUT_50Hz => clk_50Hz,
                CLK_OUT_AN => clk_an,
                CLK_OUT_STATE => clk_state
                );
 --
 pwm_map: PWM
-    port map ( CLK_IN => clk_16Hz,
+    port map ( CLK_IN => clk_50Hz,
                PWM_LEVEL => pwm_level,
                PWM_OUT => pwm_out
                );  
@@ -162,28 +170,28 @@ Converter: Binary_to_decimal
 --      end if;
 --   end process SYNC_PROC;
 --Counts for the 16 second timer
-timer1: process(CLK_IN)
+timer1: process(CLK_IN, clk_slow)
     begin
-        if BTNC = '0' then
+        if button_center = '0' then
             if timer > "00000" then
-                if(rising_edge(CLK_SLOW)) then 
+                if(rising_edge(clk_slow)) then 
                     timer <= timer - 1; 
                 end if;
             end if;
-        elsif BTNC = '1' then
+        elsif button_center = '1' then
             timer <= "10000"; 
         end if;
     end process timer1;
 --
-timer2: process(CLK_IN)
+timer2: process(CLK_IN, clk_16Hz)
     begin
-        if BTNC = '0' then
+        if button_center = '0' then
             if pwm_level /= x"ff" then
                 if(rising_edge(clk_16Hz)) then 
                     pwm_level <= pwm_level + '1';
                 end if;
             end if;
-        elsif BTNC = '1' then
+        elsif button_center = '1' then
                pwm_level <= x"00";  
         end if;
     end process timer2;
@@ -194,6 +202,8 @@ begin
         when "10000" => --16
             -- 0%
             LED(8 downto 7) <= pwm_out & pwm_out;
+            LED(15 downto 9) <= "0000000";
+            LED(6 downto 0) <= "0000000";
         when "01111" => --15
             -- 6.25%
             LED(8 downto 7) <= pwm_out & pwm_out;  
@@ -258,112 +268,5 @@ begin
         when others => null;
   end case;
 end process output_driver;
-
-----Chooses the next state depending on button presses
---NEXT_STATE_DECODE: process (state, BTNC, BTNU, BTND, BTNL, BTNR)
---begin
---    next_state <= state;  
-  
---    --state case statement to change a state on rising edge  
---    case (state) is
---        when st1_wait =>
---            if BTNC = '0' then
---                next_state <= st2_6_25;
---            end if;
---        when st2_6_25 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 13 then
---                next_state <= st2_12_5;
---            end if;
---        when st2_12_5 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 12 then
---                next_state <= st2_18_75;
---            end if;
---        when st2_18_75 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 11 then
---                next_state <= st2_25;
---            end if;
---        when st2_25 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 10 then 
---                next_state <= st2_31_25;
---            end if;
---        when st2_31_25 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 9 then 
---                next_state <= st2_37_5;
---            end if;
---        when st2_37_5 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 8 then
---                next_state <= st2_43_75;
---            end if;
---        when st2_43_75 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 7 then
---                next_state <= st2_50;
---            end if;
---        when st2_50 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 6 then
---                next_state <= st2_56_25;
---            end if;        
---        when st2_56_25 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 5 then
---                next_state <= st2_62_5;
---            end if;
---        when st2_62_5 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 4 then
---                next_state <= st2_75;
---            end if;
---        when st2_75 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 3 then
---                next_state <= st2_81_25;
---            end if;
---        when st2_81_25 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 2 then 
---                next_state <= st2_87_5;
---            end if;
---        when st2_87_5 =>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 1 then
---                next_state <= st2_93_75;
---            end if;
---        when st2_93_75=>
---            if  BTNC = '1' then
---                next_state <= st1_wait;
---            elsif timer = 0 then
---                next_state <= st2_100;
---            end if;
---        when st2_100 =>
---            if  BTNC = '1' then
---               next_state <= st1_wait;
---            end if;     
---        when others =>
---            next_state <= st1_wait;
-            
---    end case;      
---end process;
-                
-
 
 end Behavioral;
